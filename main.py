dd784de0b715aa347a582c085f4b176b09dae727
Luke Bowen <lbowen3@students.avc.edu>
2023-10-23 08:41:55 -0700

    Changed code

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
-    m_perc = int(input('Input number of male students: '))
-    f_perc = int(input('Input number of female students: '))
+    m_num = int(input('Input number of male students: '))
+    f_num = int(input('Input number of female students: '))
 
-    total = m_perc + f_perc
+    total = m_num + f_num
+
+    m_perc = m_num/total * 100
+    f_perc = f_num/total * 100
 
     print(f'The total number of students is {total}')
-    print(f'The total number of female students is {f_perc}, and the total number of male students is {m_perc}')
-    print(f'The percentage of male students is {m_perc/total:.2%} and the percentage of female students is {f_perc/total:.2%}')
-    
+    print(f'The total number of male students is {m_num}, and the total number of female students is {f_num}')
+    print(f'The percentage of male students is {m_perc:.2f} and the percentage of female students is {f_perc:.2f}')
+
     """
     ########################################
     # Do not delete the return statement
